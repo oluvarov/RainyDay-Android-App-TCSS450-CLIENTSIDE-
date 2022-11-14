6fdf03e0bbe1fb4fea3375f6325fe39c570f83ee
Sam Huynh <thecho12@hotmail.com>
2022-11-13 17:59:12 -0800

    Removed commented out code in ChangeNameViewModel. Attempt at having HomeFragment displaying user's first and last name by requesting user's info through API. Still needs work.

--- app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/home/HomeFragment.java
@@ -7,37 +7,108 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.uw.tcss450.shuynh08.tcss450clientside.MainActivity;
 import edu.uw.tcss450.shuynh08.tcss450clientside.R;
+import edu.uw.tcss450.shuynh08.tcss450clientside.databinding.FragmentChangeNameBinding;
 import edu.uw.tcss450.shuynh08.tcss450clientside.databinding.FragmentHomeBinding;
 import edu.uw.tcss450.shuynh08.tcss450clientside.model.UserInfoViewModel;
+import edu.uw.tcss450.shuynh08.tcss450clientside.ui.account.changename.ChangeNameViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private String mName;
+
+    private FragmentHomeBinding binding;
+
+    private HomeViewModel mHomeModel;
+
+    private UserInfoViewModel model;
+
     public HomeFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHomeModel = new ViewModelProvider(getActivity())
+                .get(HomeViewModel.class);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        UserInfoViewModel model = new ViewModelProvider(getActivity())
+        model = new ViewModelProvider(getActivity())
                 .get(UserInfoViewModel.class);
 
-        FragmentHomeBinding.bind(getView()).textHello.setText("Hello " + model.getEmail());
+        mName = model.getEmail();
+
+        mHomeModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse);
+
+        attemptGetInfo();
+
+        binding.textHello.setText("Hello " + mName);
+        //FragmentHomeBinding.bind(getView()).textHello.setText("Hello " + mName);
+        System.out.println(model.getmJwt());
+    }
+
+    private void attemptGetInfo() {
+        mHomeModel.connect(model.getmJwt());
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    System.out.println("UNEXPECTED");
+                    Snackbar snackbar = Snackbar.make(binding.textHello,
+                            "Error Authenticating: " +
+                            response.getJSONObject("data").getString("message"),
+                            Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                try {
+                    mName = response.getString("firstname") +
+                            " " + response.getString("lastname");
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+
     }
 }
--- app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/home/HomeViewModel.java
+++ app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/home/HomeViewModel.java
@@ -1,4 +1,4 @@
-package edu.uw.tcss450.shuynh08.tcss450clientside.ui.account.changename;
+package edu.uw.tcss450.shuynh08.tcss450clientside.ui.home;
 
 import android.app.Application;
 import android.util.Base64;
@@ -9,13 +9,11 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,11 +23,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class ChangeNameViewModel extends AndroidViewModel {
+import edu.uw.tcss450.shuynh08.tcss450clientside.io.RequestQueueSingleton;
+
+public class HomeViewModel extends AndroidViewModel {
 
     private MutableLiveData<JSONObject> mResponse;
 
-    public ChangeNameViewModel(@NonNull Application application) {
+    public HomeViewModel(@NonNull Application application) {
         super(application);
         mResponse = new MutableLiveData<>();
         mResponse.setValue(new JSONObject());
@@ -65,41 +65,37 @@ public class ChangeNameViewModel extends AndroidViewModel {
     }
 
     /**
-     * User inputs their first and last name. Info is sent to the API
-     * and changes their names in the database.
-     * @param first
-     * @param last
+     *  User inputs their email and password to sign in, info is sent to API to check
+     *  whether user is registered
      * @param jwt
      */
-    public void connect(final String first,
-                        final String last,
-                        final String jwt) {
-        String url = "https://tcss450-weather-chat.herokuapp.com/user/update/name";
+    public void connect(final String jwt) {
+        String url = "https://tcss450-weather-chat.herokuapp.com/user";
 
         Request request = new JsonObjectRequest(
-                Request.Method.POST,
+                Request.Method.GET,
                 url,
-                null,
+                null, //no body for this get request
                 mResponse::setValue,
                 this::handleError) {
+
 
             @Override
             public Map<String, String> getHeaders() {
                 Map<String, String> headers = new HashMap<>();
-                // add headers <key,value>
                 headers.put("Authorization", "Bearer "  + jwt);
-                headers.put("firstname", first);
-                headers.put("lastname", last);
                 return headers;
             }
         };
+
 
         request.setRetryPolicy(new DefaultRetryPolicy(
                 10_000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Instantiate the RequestQueue and add the request to the queue
-        Volley.newRequestQueue(getApplication().getApplicationContext())
-                .add(request);
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
     }
+
 }
