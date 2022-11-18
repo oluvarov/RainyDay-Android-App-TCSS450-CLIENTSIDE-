b89b2340a5d8b6ba41c269c612e99f6551b639f1
Sam Huynh <thecho12@hotmail.com>
2022-11-17 21:01:49 -0800

    Start to work on weather api.

--- app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/weather/WeatherFragment.java
@@ -2,37 +2,101 @@ package edu.uw.tcss450.shuynh08.tcss450clientside.ui.weather;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.uw.tcss450.shuynh08.tcss450clientside.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.tcss450.shuynh08.tcss450clientside.databinding.FragmentWeatherBinding;
+import edu.uw.tcss450.shuynh08.tcss450clientside.model.UserInfoViewModel;
+
 
 public class WeatherFragment extends Fragment {
 
-    private WeatherViewModel mViewModel;
+    private FragmentWeatherBinding binding;
 
-    public static WeatherFragment newInstance() {
-        return new WeatherFragment();
+    private WeatherViewModel mWeatherModel;
+
+    public WeatherFragment() {
+        // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWeatherModel = new ViewModelProvider(getActivity())
+                .get(WeatherViewModel.class);
+
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
-        // TODO: Use the ViewModel
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentWeatherBinding.inflate(inflater);
+        // Inflate the layout for this fragment
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Context context = requireContext().getApplicationContext();
+        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        binding.getIPAddress.setText("Your Device IP Address: " + ip);
+        System.out.println(ip);
+
+        mWeatherModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse);
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.editEmail.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                try {
+                    mUserViewModel = new ViewModelProvider(getActivity(),
+                            new UserInfoViewModel.UserInfoViewModelFactory(
+                                    binding.editEmail.getText().toString(),
+                                    response.getString("token")
+                            )).get(UserInfoViewModel.class);
+
+                    sendPushyToken();
+
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
     }
 
 }
