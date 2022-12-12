00f1f2694b942d288592b7d58ab228fe488b57c6
Sam Huynh <thecho12@hotmail.com>
2022-12-11 22:11:54 -0800

    Chat messages show email as well. Attempting to show accept button for incoming requests.

--- app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/chat/ChatRecyclerViewAdapter.java
+++ app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/chat/ChatRecyclerViewAdapter.java
@@ -102,7 +102,8 @@ public class ChatRecyclerViewAdapter extends RecyclerView.Adapter<ChatRecyclerVi
             } else {
                 //This message is from another user. Format it as such
                 binding.textMessage.setText(message.getFirstName() + " " + message.getLastName() +
-                        ": \n" + message.getMessage());
+                        ": \n" + message.getMessage() +
+                        "\n" + message.getSender());
                 ViewGroup.MarginLayoutParams layoutParams =
                         (ViewGroup.MarginLayoutParams) card.getLayoutParams();
 
