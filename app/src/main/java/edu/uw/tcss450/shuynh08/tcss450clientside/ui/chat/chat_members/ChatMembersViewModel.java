81fd49475ea15f2fe7aac109cd15f54100d7a6b5
Timon Tukei <timon.l.tukei@gmail.com>
2022-12-11 22:17:01 -0800

    Implemented removing user from chat

---
+++ app/src/main/java/edu/uw/tcss450/shuynh08/tcss450clientside/ui/chat/chat_members/ChatMembersViewModel.java
@@ -0,0 +1,96 @@
+package edu.uw.tcss450.shuynh08.tcss450clientside.ui.chat.chat_members;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import edu.uw.tcss450.shuynh08.tcss450clientside.io.RequestQueueSingleton;
+
+public class ChatMembersViewModel extends AndroidViewModel {
+
+    private MutableLiveData<JSONObject> mChatMember;
+
+    public ChatMembersViewModel(@NonNull Application application) {
+        super(application);
+        mChatMember = new MutableLiveData<>();
+        mChatMember.setValue(new JSONObject());
+    }
+
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer){
+        mChatMember.observe(owner, observer);
+    }
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mChatMember.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset())
+                    .replace('\"', '\'');
+            try {
+                mChatMember.setValue(new JSONObject("{" +
+                        "code:" + error.networkResponse.statusCode +
+                        ", data:\"" + data +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
+
+    public void connectRemoveMember(String email, String chatID, final String jwt) {
+
+        String url = "https://tcss450-weather-chat.herokuapp.com/chats/" + chatID + "/" + email;
+        Log.i("info",jwt);
+
+        Request request = new JsonObjectRequest(
+                Request.Method.DELETE,
+                url,
+                null,
+                mChatMember::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+
+
+    }
+}
